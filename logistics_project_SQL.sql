7dce7dff92bf2fdac0640363e986e75e05514034
erdalakoti121 <er.dalakoti123@gmail.com>
2026-06-01 17:05:13 +0530

    logistics_project_SQL.sql

--- logistics_project_SQL.sql
+++ logistics_project_SQL.sql
@@ -54,7 +54,7 @@ select warehouse_city, count(*) as shipment_volume
 from shipments_data
 group by warehouse_city order by shipment_volume desc;
 
--- Q7 warehouse contributing highest logistics cost.alter
+-- Q7 warehouse contributing highest logistics cost.
 
 select warehouse_city, sum(transport_cost) as max_logistics_cost
 from shipments_data group by warehouse_city
@@ -108,6 +108,11 @@ having avg(transport_cost)> (select avg(transport_cost) from shipments_data);
 -- PHASE 4 ;  FINAL ONE; real business questions.
 
 -- Q 15 calculate on time delivery %. 
+SELECT
+  ROUND(COUNT(CASE WHEN shipment_status = 'Delivered'
+  AND DATEDIFF(delivery_date, shipment_date) <= 5
+  THEN 1 END) * 100.0 / COUNT(*), 2) AS otd_pct
+FROM shipments_data;
 
 -- Q 16 find monthly shipment trends. 
 
@@ -118,7 +123,8 @@ group by monthlytrend order by totalcount desc;
 -- Q 17 Find top 3 costly shipments per warehouse.
 
 select * from (
-select warehouse_city, transport_cost, row_number() over(partition by warehouse_city) as max_cost from shipments_data) t 
+select warehouse_city, transport_cost, row_number() over(partition by warehouse_city) 
+as max_cost from shipments_data) t 
 where max_cost <= 3;
 
 -- Q18 Find customer cities receiving maximum shipments.
@@ -130,7 +136,7 @@ group by customer_city order by max_shipments desc limit 1;
 -- Q19 How much each warehouse contributes to total company fuel cost.
 
 select warehouse_city, sum(fuel_cost) as fuel_warehouse, 
-sum (fuel_cost) / (select sum(fuel_cost) from shipments_data)
+round (sum (fuel_cost)*100 / (select sum(fuel_cost) from shipments_data),2)
 from shipments_data
 group by warehouse_city;
 
@@ -139,4 +145,22 @@ group by warehouse_city;
 select round(count(case when  shipment_status='cancelled' then 1 end)*100/count(*),2) as cancel
 from shipments_data;
 
--- finish
+-- Q21 find a vehicle efficiency comaprision.
+
+select vehicle_type,
+  count(*) AS shipments,
+  round(AVG(transport_cost/distance_km),3) AS avg_cost_per_km,
+  round(AVG(DATEDIFF(delivery_date,shipment_date)),1) AS avg_days
+from shipments_data
+group by vehicle_type
+order by avg_cost_per_km ASC;
+
+-- Q22 find a monthly trend of cost vs shipments.
+select 
+  DATE_FORMAT(shipment_date,'%Y-%m') AS month,
+  COUNT(*) AS shipments,
+  ROUND(SUM(transport_cost),0) AS total_cost,
+  ROUND(AVG(transport_cost),0) AS avg_cost
+FROM shipments_data
+GROUP BY month
+ORDER BY month;
